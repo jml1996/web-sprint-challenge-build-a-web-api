3b8c716f931f71d3d7815b7c0b3d2a2682ac8d89
Joshua Lovins <joshualovins@Joshuas-MacBook-Pro.local>
2021-01-15 00:07:44 -0500

    Landning page endpoint

--- api/server.js
+++ api/server.js
@@ -12,5 +12,8 @@ server.use('/api/projects', ProjectsRouter)
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+server.get('/', (req, res) => {
+    res.send(`<h2>For data, navigate to either of these two endpoints: (1) [url]/api/projects, and (2) [url]/api/actions.</h2>`)
+})
 
 module.exports = server;
