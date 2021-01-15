47201c6d5d4bf51a79934a3a12110bce3c4145b2
Joshua Lovins <joshualovins@Joshuas-MacBook-Pro.local>
2021-01-14 23:24:11 -0500

    projects-router endpoints and middleware finished and tested

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -9,20 +9,44 @@ async function validateActionId(req, res, next) {
         req.action = action
         next()
       } else {
-        res.status(404).json({ message: `Action with id ${req.params.id} not found.` })
+        res.status(404).json({ message: `Action with id ${req.params.id} not found` })
       }
     } catch (error) {
-      res.status(500).json({ error: 'Error getting action by id.' })
+      res.status(500).json({ error: 'Error getting action by id' })
     }
 }
 
 function validateAction(req, res, next) {
     console.log('checking action request body')
     if (!req.body.project_id || !req.body.description || !req.body.notes) {
-        res.status(400).json({ message: "project_id, description, and notes are required fields" })
+        res.status(400).json({ message: 'project_id, description, and notes are required action fields' })
     } else {
         next()
     }
 }
 
-module.exports = { validateActionId, validateAction }
+async function validateProjectId(req, res, next) {
+    console.log('checking project id')
+    try {
+        const project = await Projects.get(req.params.id)
+        if (project) {
+            req.project = project
+            next()
+        } else {
+            res.status(404).json({ message: `Project with id ${req.params.id} not found`})
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error getting project by id' })
+    }
+}
+
+function validateProject(req, res, next) {
+    console.log('checking project request body')
+    if (!req.body.name || !req.body.description) {
+        res.status(400).json({ message: 'name and description are required project fields' })
+    } else {
+        next()
+    }
+}
+
+module.exports = { validateActionId, validateAction, validateProjectId, validateProject }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const Projects = require('./projects-model')
 
+const { validateProjectId, validateProject } = require('../middleware/middleware.js')
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
@@ -12,6 +14,51 @@ router.get('/', (req, res) => {
             console.log(error)
             res.status(500).json({
                 message: 'Error getting all projects'
+            })
+        })
+})
+
+router.get('/:id', validateProjectId, (req, res) => {
+    // Are actions considered part of projects here?
+    // If so, why here and not in get('/')?
+    res.status(200).json(req.project)
+})
+
+router.post('/', validateProject, (req, res) => {
+    Projects.insert(req.body)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({
+                message: 'Error posting new project'
+            })
+        })
+})
+
+router.put('/:id', validateProjectId, validateProject, (req, res) => {
+    Projects.update(req.params.id, req.body)
+    .then(updated => {
+        res.status(200).json(updated)
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({
+            message: 'Project update failed'
+        })
+    })
+})
+
+router.delete('/:id', validateProjectId, (req, res) => {
+    Projects.remove(req.params.id)
+        .then(() => {
+            res.status(200).json()
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({
+                message: 'Error deleting project'
             })
         })
 })
