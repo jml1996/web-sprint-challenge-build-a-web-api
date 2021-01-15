ee244e32b0731270a8581d0b9de8c9daeea3424e
Joshua Lovins <joshualovins@Joshuas-MacBook-Pro.local>
2021-01-15 00:04:21 -0500

    So, to be clear, did all three stretches. Not sure where to put the heroku link.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -32,6 +32,7 @@ router.get('/:id', validateProjectId, (req, res) => {
 router.get('/:id/actions', validateProjectId, (req, res) => {
     // Here
     res.status(200).json(req.project.actions)
+    // Here
 })
 
 router.post('/', validateProject, (req, res) => {
